a4d0a8347026e8a072638a312d18942e416b3ad5
liubas3171 <54552855+liubas3171@users.noreply.github.com>
2020-09-28 12:46:23 +0300

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,24 +1,47 @@
-def binary_search(element, array):
+def elem_in_array(element, array):
     '''
     :param element: int
     :param array: sorted list of ints
     :return: True if element is in array, False otherwise
     '''
-    floor = 0
-    ceil = len(array)
-    while (ceil - floor) > 1:
-        pointer = (ceil + floor) // 2
-        if array[pointer] == element:
-            return True
-        elif array[pointer] < element:
-            floor = pointer
+
+    length = len(array)
+    fib_num_2_back = 0
+    fib_num_1_back = 1
+    fib_num = fib_num_2_back + fib_num_1_back
+
+    while (fib_num < length):
+        fib_num_2_back = fib_num_1_back
+        fib_num_1_back = fib_num
+        fib_num = fib_num_2_back + fib_num_1_back
+
+    floor_pointer = -1
+
+    while (fib_num > 1):
+        index = min(floor_pointer + fib_num_2_back, length - 1)
+
+        if (array[index] < element):
+            fib_num = fib_num_1_back
+            fib_num_1_back = fib_num_2_back
+            fib_num_2_back = fib_num - fib_num_1_back
+            floor_pointer = index
+
+        elif (array[index] > element):
+            fib_num = fib_num_2_back
+            fib_num_1_back = fib_num_1_back - fib_num_2_back
+            fib_num_2_back = fib_num - fib_num_1_back
+
         else:
-            ceil = pointer
+            return True
+
+    if (fib_num_1_back and floor_pointer + 1 < len(array) - 1) and \
+            (array[floor_pointer + 1] == element):
+        return True
+
     return False
 
 
-if __name__ == '__main__':
-    vector = [2, 4, 6, 8, 12, 13, 15]
-    print(binary_search(12, vector))
-    
-# It is binary search so its complexity is O(logn)
+if __name__ == "__main__":
+    array = [2, 5, 8, 14, 22, 33, 48, 59, 60, 62]
+    el = 5
+    print(elem_in_array(el, array))
